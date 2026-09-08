3f93d9ae065be0c2310932ab4e1233780a009c02
matt <m.b.smith-5@umcutrecht.nl>
2026-09-08 12:12:48 +0200

    Starting 1.8 development cycle.`

--- src/main/java/deformablemesh/plugins/Deforming3DMesh_Plugin.java
+++ src/main/java/deformablemesh/plugins/Deforming3DMesh_Plugin.java
@@ -45,7 +45,7 @@ import java.util.stream.IntStream;
  * Created by msmith on 12/1/15.
  */
 public class Deforming3DMesh_Plugin implements PlugInFilter {
-    public static final String version = "1.7.0";
+    public static final String version = "1.8.0-SNAPSHOT";
     public static SegmentationController createDeformingMeshApplication(){
 
         MeshFrame3D mf3d = new MeshFrame3D();
